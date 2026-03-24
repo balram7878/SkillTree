7571bec4f33921cad9a25805f4157096eb116a31
Balram Meena <bmeena97562@gmail.com>
2026-03-24 19:17:29 +0530

    fix passport issues

--- Backend/config/passport.js
+++ Backend/config/passport.js
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL: `${process.env.BASE_URL}/api/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
@@ -65,7 +65,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "/api/auth/github/callback",
+      callbackURL: `${process.env.BASE_URL}/api/auth/github/callback`,
       scope: ["user:email"], // GitHub needs explicit email scope
     },
     async (accessToken, refreshToken, profile, done) => {
