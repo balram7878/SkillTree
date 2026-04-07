e79b7afba0f363ce2bc1e8cdfffab16488ac0f99
Balram Meena <bmeena97562@gmail.com>
2026-04-07 22:49:18 +0530

    fix sidebar

--- Frontend/src/pages/dashboard/Sidebar.jsx
+++ Frontend/src/pages/dashboard/Sidebar.jsx
@@ -13,7 +13,7 @@ export default function Sidebar() {
 
   return (
     <>
-      <aside className="w-64 bg-white border-r border-[#E8DDD0] hidden md:flex flex-col py-6 shrink-0 z-10 max-h-screen">
+      <aside className="w-64 bg-white border-r border-[#E8DDD0] hidden md:flex flex-col py-6 shrink-0 z-10 max-h-screen rounded-xl">
         <nav className="flex-1 space-y-2 px-4">
           <button className="w-full flex items-center gap-3 px-4 py-3 text-left rounded-xl bg-[#F5F0EB] text-[#F97316] font-bold shadow-sm transition-all hover:opacity-90">
             <LayoutDashboard className="w-5 h-5 flex-shrink-0" />
@@ -37,10 +37,10 @@ export default function Sidebar() {
           </button>
         </nav>
 
-        <div className="px-4 mt-auto">
+        <div className="px-4 mt-auto border-t border-[#E8DDD0] m-auto">
           <button
             onClick={logout}
-            className="w-full flex items-center gap-3 px-4 py-3 text-left rounded-xl text-[#6B6B6B] hover:bg-red-50 hover:text-red-700 font-medium transition-all"
+            className="w-full mt-1 flex items-center gap-3 px-4 py-3 text-left rounded-xl text-[#6B6B6B] hover:bg-red-50 hover:text-red-700 font-medium transition-all"
           >
             <LogOut className="w-5 h-5 flex-shrink-0" />
             Logout
